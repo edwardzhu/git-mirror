cbedcecacc083dc9d28b4a2ede7dd617f7a15edf
Edward Zhu <edward.zr.zhu@gmail.com>
2019-06-12 20:56:01 +0800

    Trim the end '\r'.

--- bash.sh
+++ bash.sh
@@ -2,16 +2,16 @@
 input=$GIT_LIST
 for f in *.git; do
 	if [ -d "$f" ]; then
-		cd $f && git fetch origin && git lfs fetch --all && git fetch --tags && cd ..
+		cd $f && git fetch origin && git lfs fetch --all && git fetch --tags; cd ..
 	fi
 done
 while IFS= read -r line
 do
 	echo "$line"
-	folder=$(basename $line)
+	folder=$(basename $line | tr -d '\r')
 	if [ ! "$(find "$folder" -mindepth 1 -print -quit 2>/dev/null)" ]; then
 		git clone --mirror $line && \
-		cd $folder && git lfs fetch --all && cd .. && \
+		cd $folder && git lfs fetch --all; cd .. && \
 		echo "Cloned: $line"
 	fi
 done < "$input"
